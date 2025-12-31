5af25c6b2f52e1397151193c444b3cd5bab1f84f
Edward Dobrusin <edwardobrusin@gmail.com>
2025-12-31 09:56:27 -0600

    Nueva versión dashboard

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -42,7 +42,7 @@ def calculate_hts_sum(row):
 
 @st.cache_data
 def load_data():
-    path = 'data/manual/LIGIE_HTS_Dashboard.xlsx'
+    path = 'data/manual/LIGIE_HTS_Dashboard_v3.xlsx'
     
     # 1. Carga LIGIE
     ligie = pd.read_excel(path, sheet_name='LIGIE', dtype=str)
